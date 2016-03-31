eea97e3e961cb5e7e51175b791a09833f38fd3e8
taoyuan <torworx@gmail.com>
2016-03-31 22:16:05 +0800

    Using tessocr as ocr utility

---
+++ index.js
@@ -0,0 +1,3 @@
+'use strict';
+
+exports.recognize = require('./lib/recognize');
--- test/recognize.test.js
+++ test/recognize.test.js
@@ -8,35 +8,35 @@ var Jimp = Promise.promisifyAll(require('jimp'));
 var npos = require('npos');
 var raster = npos.codecs.raster;
 
-var ocr = require('..').ocr;
+var recognize = require('..').recognize;
 
 var imageFile = path.join(__dirname, 'fixtures', 'receipt.png');
 var rasterFile = path.join(__dirname, 'fixtures', 'raster.bin');
 
-describe('ocr', function () {
+describe('recognize', function () {
   this.timeout(5000);
 
-  it('should ocr using local image file path', function () {
-    return ocr(imageFile).then(function (text) {
+  it('should recognize using local image file path', function () {
+    return recognize(imageFile).then(function (text) {
       assert.ok(text);
       assert.include(text, '单号');
     });
   });
 
-  it('should ocr using png image', function () {
-    return Jimp.readAsync(imageFile).then(ocr).then(function (text) {
+  it('should recognize using png image', function () {
+    return Jimp.readAsync(imageFile).then(recognize).then(function (text) {
       assert.ok(text);
       assert.include(text, '单号');
     });
   });
 
-  it('should ocr with npos BitImage', function () {
+  it('should recognize with npos BitImage', function () {
     var image = npos.bitimage();
     var i = fs.readFileSync(rasterFile);
     for (i = raster.decode(i); i; i = raster.decode(i)) {
       image.append(i);
     }
-    return ocr(image).then(function (text) {
+    return recognize(image).then(function (text) {
       assert.ok(text);
       assert.include(text, '单号');
     });
